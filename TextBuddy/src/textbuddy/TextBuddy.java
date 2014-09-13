1b3b722d9f42994db293e1dc21e14dccb485f510
Jireh <twajxjiraffe@gmail.com>
2014-09-13 14:07:48 +0800

    refactor: changed general imports to specific imports.

--- TextBuddy/src/textbuddy/TextBuddy.java
+++ TextBuddy/src/textbuddy/TextBuddy.java
@@ -6,10 +6,17 @@
  */
 package textbuddy;
 
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class TextBuddy {
 
